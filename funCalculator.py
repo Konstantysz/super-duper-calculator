67447140528f612e47eaacc46f8c07320c6419a5
bhrylski <74931101+bhrylski@users.noreply.github.com>
2021-07-12 22:32:20 +0200

    spacing and variable cleanup

--- funCalculator.py
+++ funCalculator.py
@@ -1,30 +1,18 @@
 import math 
-import mathFun
-
-#first input -> num1 
-#opperation -> opp
-#second input -> num2
-#perform calculation & result -> res
-# ask for next opperation
-# if "=" then print res 
-# else res -> num1 
-# ask num2 
-# perform caluculation & result -> res  
+import mathFun 
 
 def funCalculator():
    
     strInput = input()
-    num = 0
+    res = 0
     if strInput == "pi":
-        num = float(math.pi)
+        res = float(math.pi)
     elif strInput == "e":
-        num = float(math.e)
+        res = float(math.e)
     elif strInput == "tau":
-        num = float(math.tau)
+        res = float(math.tau)
     else:
-        num = float(strInput)
-
-    res = num
+        res = float(strInput)
 
     while True:
         
@@ -45,17 +33,17 @@ def funCalculator():
             num = float(strInput)
 
         if opp == "+":
-            res = mathFun.add(res,num)
+            res = mathFun.add(res, num)
         elif opp == "-":
-            res = mathFun.subtract(res,num)    
+            res = mathFun.subtract(res, num)    
         elif opp == "x":
-            res=mathFun.multiply(res,num)
+            res=mathFun.multiply(res, num)
         elif opp == "/":
-            res = mathFun.devide(res,num)
+            res = mathFun.devide(res, num)
         elif opp == "%":
-            res = mathFun.remainder(res,num)
+            res = mathFun.remainder(res, num)
         elif opp == "*":
-            res = mathFun.exponent(res,num)
+            res = mathFun.exponent(res, num)
     
     print(res)
        
