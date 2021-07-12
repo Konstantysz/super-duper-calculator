238e4babec186a7ae2a285b102661a74bbc593aa
bhrylski <74931101+bhrylski@users.noreply.github.com>
2021-07-12 22:01:12 +0200

    Added loop for multiple inputs

--- funCalculator.py
+++ funCalculator.py
@@ -3,7 +3,6 @@ import mathFun
 
 def funCalculator():
    
-
     strInput = input()
     num1 = 0
     if strInput == "pi":
@@ -42,10 +41,44 @@ def funCalculator():
         res = mathFun.remainder(num1,num2)
     elif opp == "*":
         res = mathFun.exponent(num1,num2)
-    opp = input()
 
-    if opp == "=":
-        print(res)
+    while True:
+        
+        opp = input()
+        
+        if opp == "=": 
+            break
+
+        num1 = res
+
+        strInput = input()
+        num2 = 0
+        if strInput == "pi":
+            num2 = float(math.pi)
+        elif strInput == "e":
+            num2 = float(math.e)
+        elif strInput == "tau":
+            num2 = float(math.tau)
+        else:
+            num2 = float(strInput)
+
+        res = 0
+
+        if opp == "+":
+            res = mathFun.add(num1,num2)
+        elif opp == "-":
+            res = mathFun.subtract(num1,num2)    
+        elif opp == "x":
+            res=mathFun.multiply(num1,num2)
+        elif opp == "/":
+            res = mathFun.devide(num1,num2)
+        elif opp == "%":
+            res = mathFun.remainder(num1,num2)
+        elif opp == "*":
+            res = mathFun.exponent(num1,num2)
+    
+    print(res)
+       
         
 if __name__ == '__main__':
     funCalculator()
