bbda3e3eb18d73d70646272522a47892f4c7448f
bhrylski <74931101+bhrylski@users.noreply.github.com>
2021-07-13 16:33:10 +0200

    Implemented Memory

--- funCalculator.py
+++ funCalculator.py
@@ -2,50 +2,56 @@ import math
 import mathFun 
 
 def funCalculator():
-   
-    strInput = input()
+    mem = 0 
     res = 0
-    if strInput == "pi":
-        res = float(math.pi)
-    elif strInput == "e":
-        res = float(math.e)
-    elif strInput == "tau":
-        res = float(math.tau)
-    else:
-        res = float(strInput)
-
     while True:
-        
-        opp = input()
-        
-        if opp == "=": 
-            break
-
+        mem = res
         strInput = input()
-        num = 0
+        res = 0
         if strInput == "pi":
-            num = float(math.pi)
+            res = float(math.pi)
         elif strInput == "e":
-            num = float(math.e)
+            res = float(math.e)
         elif strInput == "tau":
-            num = float(math.tau)
+            res = float(math.tau)
         else:
-            num = float(strInput)
+            res = float(strInput)
 
-        if opp == "+":
-            res = mathFun.add(res, num)
-        elif opp == "-":
-            res = mathFun.subtract(res, num)    
-        elif opp == "x":
-            res=mathFun.multiply(res, num)
-        elif opp == "/":
-            res = mathFun.devide(res, num)
-        elif opp == "%":
-            res = mathFun.remainder(res, num)
-        elif opp == "*":
-            res = mathFun.exponent(res, num)
-    
-    print(res)
+        while True:
+            
+            opp = input()
+            
+            if opp == "=": 
+                break
+
+            strInput = input()
+            num = 0
+            if strInput == "pi":
+                num = float(math.pi)
+            elif strInput == "e":
+                num = float(math.e)
+            elif strInput == "tau":
+                num = float(math.tau)
+            elif strInput == "mem":
+                num = mem
+            else:
+                num = float(strInput)
+
+            if opp == "+":
+                res = mathFun.add(res, num)
+            elif opp == "-":
+                res = mathFun.subtract(res, num)    
+            elif opp == "x":
+                res=mathFun.multiply(res, num)
+            elif opp == "/":
+                res = mathFun.devide(res, num)
+            elif opp == "%":
+                res = mathFun.remainder(res, num)
+            elif opp == "^":
+                res = mathFun.exponent(res, num)
+            
+        if opp == "=":
+            print(res)
        
         
 if __name__ == '__main__':
