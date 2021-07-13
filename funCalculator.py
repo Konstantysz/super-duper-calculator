33e78c647edf91f2544e95c05c93ea230e64568e
bhrylski <74931101+bhrylski@users.noreply.github.com>
2021-07-13 16:16:20 +0200

    Revert "Added more functions and started to figure out memory"
    This reverts commit 3794c9b743eed666e2a55d6834fc328a8f546feb.

--- funCalculator.py
+++ funCalculator.py
@@ -5,22 +5,19 @@ def funCalculator():
    
     strInput = input()
     res = 0
-
     if strInput == "pi":
         res = float(math.pi)
     elif strInput == "e":
         res = float(math.e)
     elif strInput == "tau":
         res = float(math.tau)
-    # elif strInput == "mem":
-    #     res = mem
     else:
         res = float(strInput)
 
     while True:
-    
+        
         opp = input()
-    
+        
         if opp == "=": 
             break
 
@@ -45,27 +42,11 @@ def funCalculator():
             res = mathFun.devide(res, num)
         elif opp == "%":
             res = mathFun.remainder(res, num)
-        elif opp == "^":
+        elif opp == "*":
             res = mathFun.exponent(res, num)
-        elif opp == "squared":
-            res = mathFun.square(res)
-            break
-        elif opp == "inverse":
-            res = mathFun.inverse(res)
-            break
-        elif opp == "squrt":
-            res = mathFun.squrt(res)
-            break
-        # elif opp == "mem+":
-        #     mem = res
-        #     break
-
-        print(res)
-
     
-            
+    print(res)
        
-        
         
 if __name__ == '__main__':
     funCalculator()
